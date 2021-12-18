9c1cff90e6e0781c9d405edc45ea28f8724b7a37
bridzlab assignment <naveenmabbu143@gmail.com>
2021-12-18 20:13:46 +0530

    further code push

---
+++ c3q2.sh
@@ -0,0 +1,37 @@
+
+echo "Enter number:"
+read num
+
+case $num in
+  1)
+    echo "It's monday!"
+    ;;
+
+  2)
+    echo "It's tuesday!"
+    ;;
+
+  3)
+    echo "It's wednasday!"
+    ;;
+ 4)
+    echo "It's thursday!"
+    ;;
+
+  5)
+    echo "It's friday!"
+    ;;
+
+  6)
+    echo "It's saturday!"
+    ;;
+  7)
+    echo "It's sunday!"
+    ;;
+
+  *)
+    echo "It's something else!"
+    ;;
+esac
+
+echo "End of script."
